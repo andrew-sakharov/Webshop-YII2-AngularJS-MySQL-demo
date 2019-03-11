8d0116f481e844ef10b7e3dfc6d8654642bb6752
Андрей <47139248+andrew-sakharov@users.noreply.github.com>
2019-03-11 16:12:36 +0300

    Add files via upload

--- Getsorts.php
+++ Getsorts.php
@@ -2,22 +2,18 @@
 
 namespace app\repositories;
 use Yii;
+use app\models\Price;
+use app\models\Sorts;
 
 class Getsorts
 {
     public function __invoke()
     {
+
         /*
-         Здесь и далее используем Yii DAO. При использовании Yii DAO в основном используется чистый SQL и массивы PHP.
-         Как результат, это самый эффективный способ доступа к базам данных.
-         Единственный недостаток - так как синтаксис SQL может отличаться для разных баз данных,
-         используя Yii DAO  нужно приложить дополнительные усилия,
-         чтобы сделать приложение не зависящим от конкретной базы данных.
-         Достоинства. Их гораздо больше:
-         - Гарантированная миграция на новое поколение Фреймворка без переписывания программ.
-         - Нам можем показывать только те позиции справочника, которым есть соответсвие в таблице фактов. 
-         - В качестве бонуса мы получаем и показываем на странице количество позиций в таблице фактов соответствующих каждой позиции из справочника.
-         */ 
+        Вариант Yii DAO. Используется чистый SQL.Самый эффективный способ доступа к базам данных.
+        Единственный недостаток - так как синтаксис SQL может отличаться для разных баз данных, используя Yii DAO  
+        нужно приложить дополнительные усилия, чтобы сделать приложение не зависящим от конкретной базы данных.
         
         $sql = "select * from sorts where flag = 'NL'";
         $sorts = Yii::$app->db->createCommand($sql)->queryAll();
@@ -29,10 +25,23 @@ class Getsorts
             $sql_count = "select count(*) from price_list where flag = 'NL' and sorts_id = '" . $sorts_id . "'";
             $count = Yii::$app->db->createCommand($sql_count)->queryScalar();
             if ($count > 0)
-                $data[] = array("id"=>$row['id'],"name"=>$row['name'], "selected"=>'YES', "count"=>$count
-                );
+                $data[] = array("id"=>$row['id'],"name"=>$row['name'], "selected"=>'YES', "count"=>$count);
                 $i++;
         }
+        */
+        
+        $sorts = Sorts::find()->where(['flag' => 'NL'])->asArray()->all();
+        $sorts_l = count($sorts);
+        $i= 0;
+        while ($i < $sorts_l) {
+            $row = $sorts[$i];
+            $sorts_id = $row['id'];
+            $count = Price::find()->where(['sorts_id' => $sorts_id])->count();
+            if ($count > 0)
+                $data[] = array("id"=>$row['id'],"name"=>$row['name'], "selected"=>'YES', "count"=>$count);
+                $i++;
+        }
+                
         return $data;
     }
 }
--- Gettypes.php
+++ Gettypes.php
@@ -2,6 +2,8 @@
 
 namespace app\repositories;
 use Yii;
+use app\models\Price;
+use app\models\Types;
 
 class Gettypes
 {
@@ -9,17 +11,10 @@ class Gettypes
     {
         
         /*
-         Здесь и далее используем Yii DAO. При использовании Yii DAO в основном используется чистый SQL и массивы PHP.
-         Как результат, это самый эффективный способ доступа к базам данных.
-         Единственный недостаток - так как синтаксис SQL может отличаться для разных баз данных,
-         используя Yii DAO  нужно приложить дополнительные усилия,
-         чтобы сделать приложение не зависящим от конкретной базы данных.
-         Достоинства. Их гораздо больше:
-         - Гарантированная миграция на новое поколение Фреймворка без переписывания программ.
-         - Нам можем показывать только те позиции справочника, которым есть соответсвие в таблице фактов
-         - В качестве бонуса мы получаем и показываем на странице количество позиций в таблице фактов соответствующих каждой позиции из справочника. 
-         */ 
-        
+        Вариант Yii DAO. Используется чистый SQL.Самый эффективный способ доступа к базам данных.
+        Единственный недостаток - так как синтаксис SQL может отличаться для разных баз данных, используя Yii DAO  
+        нужно приложить дополнительные усилия, чтобы сделать приложение не зависящим от конкретной базы данных.
+         
         $sql = "select * from types where flag = 'NL'";
         $types = Yii::$app->db->createCommand($sql)->queryAll();
         $types_l = count($types);
@@ -30,10 +25,25 @@ class Gettypes
             $sql_count = "select count(*) from price_list where flag = 'NL' and types_id = '" . $types_id . "'";
             $count = Yii::$app->db->createCommand($sql_count)->queryScalar();
             if ($count > 0)
-                $data[] = array("id"=>$row['id'],"name_nl"=>$row['name_nl'], "selected"=>'YES', "count"=>$count
-                );
+                $data[] = array("id"=>$row['id'],"name_nl"=>$row['name_nl'], "selected"=>'YES', "count"=>$count);
                 $i++;
         }
+        */       
+        
+        /*  Вариант Active Records  */
+            
+            $types = Types::find()->where(['flag' => 'NL'])->asArray()->all();
+            $types_l = count($types);
+            $i= 0;
+            while ($i < $types_l) {
+                $row = $types[$i];
+                $types_id = $row['id'];
+                $count = Price::find()->where(['types_id' => $types_id])->count();                
+                if ($count > 0)
+                    $data[] = array("id"=>$row['id'],"name_nl"=>$row['name_nl'], "selected"=>'YES', "count"=>$count);
+                $i++;
+            }
+        
         return $data;
     }
 }
